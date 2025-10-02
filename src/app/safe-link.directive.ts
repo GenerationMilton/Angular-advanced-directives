b2632ffb59dc7f0edb17d4094d52341500ab5129
Milton Munoz <livemilton@gmail.com>
2025-10-01 22:15:41 -0500

    Angular directives :sparkles: Directives To Change Element Behavior

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -2,10 +2,22 @@ import { Directive } from '@angular/core';
 
 @Directive({ 
     selector: 'a[appSafeLink]',
-    standalone: true
+    standalone: true,
+    host: {
+        '(click)':'onConfimrLeavePage($event)'
+    }
 })
 export class SafeLinkDirective {
     constructor() {
         console.log('SafeLinkDirective is active!');
+     }
+
+     onConfimrLeavePage(event: MouseEvent){
+       const wanstToLeave= window.confirm('Do you want to leave the app?');
+
+       if(wanstToLeave){
+        return;
+       }
+       event?.preventDefault();
      }
 }
