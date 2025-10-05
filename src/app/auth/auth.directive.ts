cc4f5363639ec52430cd4b4953ed9fa84abe969b
Milton Munoz <livemilton@gmail.com>
2025-10-04 19:14:19 -0500

    Angular directives :sparkles: Building a Custom Structural Directive

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, effect, inject, input } from '@angular/core';
+import { Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
@@ -9,13 +9,20 @@ import { AuthService } from './auth.service';
 export class AuthDirective {
 
   userType = input.required<Permission>({alias: 'appAuth'});
-   private authService = inject(AuthService);
+  private authService = inject(AuthService);
+  //give access to the content of the template
+  private templateRef = inject(TemplateRef);
+  //give access to the place in the DOM where this directive is being used
+  private viewContainerRef = inject(ViewContainerRef);
+
   constructor() {
     effect(()=>{
       if(this.authService.activePermission() === this.userType()){
-        console.log('SHOW ELEMENT');
+        //render some new content into a certain place in the DOM
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
       } else {
-        console.log('DO NOT SHOW ELEMENT');
+        // this will remove any embedded view that has been rendered
+        this.viewContainerRef.clear();
       }
     });
    }
